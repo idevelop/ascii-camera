2ce709ba9cd20ff35894a06a1c992ec7ee945753
lukee bajaa <57724155+lil-peen@users.noreply.github.com>
2021-02-19 21:52:23 -0500

    Update ascii.js

--- script/ascii.js
+++ script/ascii.js
@@ -65,7 +65,7 @@ var ascii = (function() {
 
 	return {
 		fromCanvas: function(canvas, options = {}) {
-			Object.assign({}, options, { contrast: 128, callback: () => {} });
+			Object.assign({}, { contrast: 128, callback: () => {} }, options);
 			
 			return asciiFromCanvas(canvas, options);
 		}
